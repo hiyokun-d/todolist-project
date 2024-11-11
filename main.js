5c411a5d30efcd225377c6b3ed809fe3aaa62ff9
nothing <ddwi2504@gmail.com>
2024-11-12 02:28:13 +0700

    Make the container for todo list

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-console.log("alpen JOMOK!")
+document.querySelector('.add-button').addEventListener('click', function (e) {
+  // Create particles
+  const particles = 12;
+  const button = e.currentTarget;
+  const rect = button.getBoundingClientRect();
 
+  for (let i = 0; i < particles; i++) {
+    const particle = document.createElement('div');
+    particle.className = 'particle';
+    particle.style.setProperty('--angle', `${(i * 360 / particles)}deg`);
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    particle.style.left = `${x}px`;
+    particle.style.top = `${y}px`;
+
+    button.querySelector('.particles').appendChild(particle);
+
+    // Remove particle after animation
+    setTimeout(() => {
+      particle.remove();
+    }, 600);
+  }
+});
