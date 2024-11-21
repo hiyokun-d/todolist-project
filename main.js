e3fec1dbafd5d5884bda48bcc0d77f4f6e5ec6f3
nothing <ddwi2504@gmail.com>
2024-11-21 17:10:19 +0700

    nambahin notification untuk notifikasi aja tod

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { addingTodo } from "./adding-todo";
 import { list_arr, Todo } from "./listComponents";
+import { showNotification } from "./notification";
 
 const inputTodo = document.getElementById("todo-input")
 const addButton = document.getElementById("todo-button")
@@ -8,6 +9,19 @@ const addButton = document.getElementById("todo-button")
 // const todo = new Todo("alpen jomok")
 // todo.generate()
 // console.log(list_arr)
+
+showNotification("Apakabar bang", {
+  description: "ini cuman dummy notification doang awokaowkaowk",
+  buttons: [{
+    name: "button1",
+    class: "primary-button"
+  },
+  {
+    name: "button2",
+    class: "secondary-button"
+  }
+  ]
+})
 
 //TODO: NGESAVE SEMUA TODOLIST DISAAT PENDUKUNG PRABOWO OKE OKE GAS KAU MALAH NGESAVE DATA USER ITU GAK RESPECT BANGET PEN
 addButton.addEventListener("click", () => {
